89c0af92f36c9197012a0cecf630edfca20976fa
Geordon Worley <vadixidav@gmail.com>
2016-03-15 03:22:57 -0400

    working; version bump

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate itertools;
 extern crate clap;
+extern crate rand;
 use itertools::Itertools;
 use clap::{App, Arg};
+use std::fs::File;
+use rand::Rng;
+use std::io::{Read, Write};
 
 const DEFAULT_CROSSOVER_POINTS: usize = 3;
 const DEFAULT_MUTATION_RATE: f64 = 0.001;
@@ -10,7 +14,7 @@ const DEFAULT_STRIDE: usize = 1;
 
 fn main() {
     let matches = App::new("matef")
-        .version("0.0")
+        .version("1.0")
         .author("Geordon Worley <vadixidav@gmail.com>")
         .about("Mates two files")
         .arg(Arg::with_name("output")
@@ -120,5 +124,98 @@ fn main() {
     };
 
     let output = matches.value_of("output").unwrap();
-    let files = (matches.value_of("file1").unwrap(), matches.value_of("file2").unwrap());
+    let filenames = (matches.value_of("file1").unwrap(), matches.value_of("file2").unwrap());
+
+    let files = (
+        match File::open(filenames.0) {
+            Ok(mut f) => {
+                let mut v = Vec::new();
+                match f.read_to_end(&mut v) {
+                    Ok(_) => {},
+                    Err(e) =>  {
+                        println!("Could not read file \"{}\": {}", filenames.0, e);
+                        return;
+                    },
+                }
+                v
+            },
+            Err(e) => {
+                println!("Could not open file \"{}\": {}", filenames.0, e);
+                return;
+            },
+        }, match File::open(filenames.1) {
+            Ok(mut f) => {
+                let mut v = Vec::new();
+                match f.read_to_end(&mut v) {
+                    Ok(_) => {},
+                    Err(e) =>  {
+                        println!("Could not read file \"{}\": {}", filenames.1, e);
+                        return;
+                    },
+                }
+                v
+            },
+            Err(e) => {
+                println!("Could not open file \"{}\": {}", filenames.1, e);
+                return;
+            },
+        },
+    );
+
+    let len = std::cmp::min(files.0.len(), files.1.len());
+
+    let mut rng = rand::os::OsRng::new().ok().unwrap();
+
+    //Generate crossover file
+    let mut result =
+        (0..crossover_points)
+        //Map these to random crossover points
+        .map(|_| rng.gen_range(0, len))
+        //Add total_instructions at the end so we can generate a range with it
+        .chain(Some(len))
+        //Sort them by value into BTree, which removes duplicates and orders them
+        .fold(std::collections::BTreeSet::new(), |mut set, i| {set.insert(i); set})
+        //Iterate over the sorted values
+        .iter()
+        //Turn every copy of two, prepending a 0, into a range
+        .scan(0, |prev, x| {let out = Some(*prev..*x); *prev = *x; out})
+        //Enumerate by index to differentiate odd and even values
+        .enumerate()
+        //Map even pairs to ranges in parent 0 and odd ones to ranges in parent 1 and expand the ranges
+        .flat_map(|(index, range)| {
+            {if index % 2 == 0 {files.0[range].iter()} else {files.1[range].iter()}}.cloned()
+        })
+        //Collect all the instruction ranges from each parent
+        .collect_vec();
+
+    //Mutate result file
+    let strides =
+        //We can only stride the beginning of a mutation group up to this actual len
+        (result.len() - (mutation_size - 1))
+        //Divide by stride
+        / stride;
+
+    for i in 0..strides {
+        if rng.next_f64() < mutation_rate {
+            for v in &mut result[(i * stride)..(i * stride + mutation_size)] {
+                *v = rng.gen();
+            }
+        }
+    }
+
+    let mut outfile = match File::create(output) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Could not create file \"{}\": {}", output, e);
+            return;
+        },
+    };
+
+    match outfile.write_all(&result[..]) {
+        Ok(_) => {},
+        Err(e) => {
+            println!("Could not write to \"{}\": {}", output, e);
+            return;
+        },
+    }
 }
